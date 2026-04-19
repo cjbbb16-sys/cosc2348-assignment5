2f559ae50da8cd31ab7f68bf21bc43b99ae8874c
cj <cjbbb16@gmail.com>
2026-04-19 23:17:28 +0000

    finished

--- calculator/tests/your_tests.py
+++ calculator/tests/your_tests.py
@@ -2,6 +2,17 @@
 from calculator_adapter import run
 
 
+import subprocess
+
+def test_addition():
+    result = subprocess.run(["./calculator", "1", "+", "1"], capture_output=True, text=True)
+    assert "2" in result.stdout
+
+def test_multiplication():
+    result = subprocess.run(["./calculator", "2", "+", "2"], capture_output=True, text=True)
+    assert "4" in result.stdout
+
+
 ### ADD AT LEAST TWO TESTS HERE!
 
 
